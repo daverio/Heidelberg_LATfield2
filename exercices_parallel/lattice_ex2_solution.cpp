b3c6ea709c0e006c290fd1d8d093137e576ebaa5
David Daverio <daverio@pi213a.itp.uni-heidelberg.de>
2016-01-19 11:35:55 +0100

    exercice 2,3

--- exercices_parallel/lattice_ex2_solution.cpp
+++ exercices_parallel/lattice_ex2_solution.cpp
@@ -31,16 +31,50 @@ int main(int argc, char **argv)
     parallel.initialize(n,m);
 
     COUT << "Parallel grid size: ("<<parallel.grid_size()[0]<<","<<parallel.grid_size()[1]<<"). "<<endl;
-
+    //-----------------------   end   ------------------------
 
 
     //------------   Declaration of a Lattice   --------------
-    int dim = 3;
-    int latSize[3] = {25,57,32};
-    int halo = 1;
-    Lattice lat(dim,latSize,halo);
+
+    Lattice lat(3,64,1);
 
     COUT << "Lattice size: ("<< lat.size(0)<<","<< lat.size(1)<<","<< lat.size(2)<<");"<<endl;
+    cout << "Process ranks: "<< parallel.rank()<<",("<< parallel.grid_rank()[0]<<","<<parallel.grid_rank()[1]<< "); ";
     cout << "Local lattice size: ("<< lat.sizeLocal(0)<<","<< lat.sizeLocal(1)<<","<< lat.sizeLocal(2)<<"); ";
+    cout << "Coordinate of the first local point: (0,"<< lat.coordSkip()[1] <<","<< lat.coordSkip()[0] <<")."<<endl;
+    //-----------------------   end   ------------------------
+
+
+    //-----------   Declaration of the Fields   --------------
+    Field<Real> rho(lat);
+
+
+    Field<Real> phi;
+    phi.initialize(lat);
+    phi.alloc();
+
+    Field<Real> gradRho(lat,3);
+    //-----------------------   end   ------------------------
+
+    //--------------   Operations on Fields   ----------------
+    Site x(lat);
+
+    for(x.first();x.test();x.next())
+    {
+        phi(x) = sin(2 * M_PI * x.coord(0) / lat.size(0));
+    }
+
+    phi.updateHalo();
+
+    double dx = 0.5;
+    for(x.first();x.test();x.next())
+    {
+
+        gradPhi(x,0) = (phi(x+0)-phi(x-0))/(dx*2.0);
+        gradPhi(x,1) = (phi(x+1)-phi(x-1))/(dx*2.0);
+        gradPhi(x,2) = (phi(x+2)-phi(x-2))/(dx*2.0);
+    }
+    //-----------------------   end   ------------------------
+
 
 }
